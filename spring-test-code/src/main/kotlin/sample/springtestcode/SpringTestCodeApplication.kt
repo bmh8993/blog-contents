c078b5f1afaf941ec9142b88dbd3ece1507c0389
bmh8993 <bmh9175@gmail.com>
2023-08-28 12:07:11 +0900

    spring test code proj init

---
+++ spring-test-code/src/main/kotlin/sample/springtestcode/SpringTestCodeApplication.kt
@@ -0,0 +1,11 @@
+package sample.springtestcode
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringTestCodeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringTestCodeApplication>(*args)
+}
